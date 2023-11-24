665d7223436b298a717afe85462740706467275c
Syleo24 <sylvain.beggiora@outlook.com>
2023-11-24 19:48:53 +0100

    page liste des questions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,42 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+
+        $questions = [
+            [
+                'title' => 'titre un',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adip',
+                'rating' => 20,
+                'author' => [
+                    'name' => 'Jean Dupont',
+                    'avatar' => 'https://randomuser.me/api/portraits/men/6.jpg'
+                ],
+                'nbrOfResponse' => 15
+            ],
+            [
+                'title' => 'titre deux',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adip, Lorem again',
+                'rating' => -15,
+                'author' => [
+                    'name' => 'Sylvie Vartant',
+                    'avatar' => 'https://randomuser.me/api/portraits/women/6.jpg'
+                ],
+                'nbrOfResponse' => 150
+            ],
+            [
+                'title' => 'titre trois',
+                'content' => 'Lorem ipsum dolores, bla bla bla bla bla ',
+                'rating' => 0,
+                'author' => [
+                    'name' => 'Julie Martin',
+                    'avatar' => 'https://randomuser.me/api/portraits/women/38.jpg'
+                ],
+                'nbrOfResponse' => 2
+            ]
+        ];
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions,
         ]);
     }
 }
