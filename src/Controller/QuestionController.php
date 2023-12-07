f9aa31074a1e5f2ef6d3b398a7a75dd2d668eef5
24Syleo <sylvain.beggiora@outlook.com>
2023-12-07 14:43:27 +0100

    ok

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -10,7 +10,6 @@ use App\Form\QuestionType;
 use App\Repository\QuestionRepository;
 use App\Repository\VoteRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
